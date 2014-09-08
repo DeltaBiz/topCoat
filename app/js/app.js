905666bd85ecf30dff899a76d3c7fe2c429cf975
Shane <shane@14oranges.com>
2014-09-08 15:29:22 -0700

    added tt.on swipe

--- app/js/app.js
+++ app/js/app.js
@@ -3,37 +3,18 @@
  */
 $(document).ready(function() {
 
-
-
     // Create the topcoatTouch object
     var tt = new TopcoatTouch({menu: [{id: 'home', name: 'Home'}, {id: 'cards', name: 'Cards'}]});
 
     // First page we go to home...  This could be done in code by setting the class to 'page page-center', but here is how to do it in code...
     tt.goTo('home');
-
-    var carouselScroll = null;
-
+	
+	tt.on('swipe', '.flip-container', function() {
+		this.classList.toggle("flip");
+	});
    $('.menuButtons').click(function() {
 		tt.goTo('cards');
    });
-
-    tt.on('click', 'button', 'help about info', function() {
-        tt.goBack();
-    });
-
-    // Show the loading message...
-    $('#showLoading').click(function() {
-        tt.showLoading('10 seconds');
-        var count = 10;
-        var interval = setInterval(function() {
-            if (--count <= 0) {
-                clearInterval(interval);
-                tt.hideLoading();
-            } else {
-                $('#topcoat-loading-message').text(count + ' seconds');
-            }
-        },1000);
-    });
 	
 	$('.cards img').click(function(event){
 		var cardId = $(this).attr('rel');
@@ -50,4 +31,5 @@ $(document).ready(function() {
         tt.showDialog('This is a dialog', 'Example Dialog', {OK: function() { console.log('OK Pressed') }
             , Cancel: function() { console.log('Cancel Pressed')}});
     });
+	
 });
